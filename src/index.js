0b034afcf57bec60feccc3730c4ab0cc65958a4e
Wesley Reed <reed.wesley.s@gmail.com>
2019-12-03 16:01:57 -0500

    added image example

--- src/index.js
+++ src/index.js
@@ -4,4 +4,5 @@ import './resources/styles/style.scss';
 $(document).ready(() => {
   const year = new Date().getFullYear();
   $(`#copyrightYear`).text(year);
+  $(`.alert`).text(`Webpack Successful!`);
 });
